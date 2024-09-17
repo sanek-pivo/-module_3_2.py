dda5df88c25a36aae0fcd07dba4707ec58ca3dbd
sanek-pivo <kapelka_555@mail.ru>
2024-09-17 20:44:25 +0700

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,20 @@
+def send_email(message,recipient,sender = "univerity.help@gmail.com"):   # создаём функцию send_email
+    if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
+        # создаём  1 условие
+        print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
+    elif recipient == sender:
+        # создаём 2 условие
+        print("Нельзя отправить письмо самому себе!")
+        # создаём 3 условие
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
+        # создаём 4 условие
+    elif sender !="university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+        send_email('messagje','university.help@mail.com','@.com')
+        send_email('messagje','university.help@mail.com')
+        send_email('messagje','universi@.com')
+        send_email('messagje','universi@.co')
+        send_email('messagje','universi@.com')
+        # вывод результата
